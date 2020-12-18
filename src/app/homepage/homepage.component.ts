32a10f0d029eb0ab8df3d26427f5a16e078d779c
charolinekandoko <charoline.kandoko@student.umn.ac.id>
2020-12-18 14:32:41 +0700

    charol commit

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -7,28 +7,28 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent implements OnInit {
-  lis: []; 
+  lis: [];
   latest: any;
   slides: any = [[]];
-  slides2: any = [[]];  
+  slides2: any = [[]];
+
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.http.get('https://api.themoviedb.org/3/movie/popular?api_key=aaeec0551acb10d5d267f42253e1a033')
-    .subscribe(Response => {
-      this.lis = Response['results'];
-      this.slides = this.chunk(this.lis,5);
-    });
+      .subscribe(Response => {
+        this.lis = Response['results'];
+        this.slides = this.chunk(this.lis, 5);
+      });
     this.http.get('https://api.themoviedb.org/3/movie/top_rated?api_key=aaeec0551acb10d5d267f42253e1a033&language=en-US&page=1')
-    .subscribe(Response => {
-      this.latest = Response['results'];
-      this.slides2 = this.chunk(this.latest,5);
-    });
-    
+      .subscribe(Response => {
+        this.latest = Response['results'];
+        this.slides2 = this.chunk(this.latest, 5);
+      });
   }
 
-  chunk(arr,chunkSize){
-    let R= [];
+  chunk(arr, chunkSize) {
+    let R = [];
     for (let i = 0, len = arr.length; i < len; i += chunkSize) {
       R.push(arr.slice(i, i + chunkSize));
     }
